532d69e6da86861bea03f8802c6ca37c0617562f
lgenteman <lydiagenteman@gmail.com>
2024-04-13 15:50:29 -0500

    React Native Week 1 Workshop-About & Contact Screens

---
+++ screens/AboutScreen.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import {Avatar, ListItem} from 'react-native-elements';
+import { ScrollView } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+
+const AboutScreen = () => {
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    );
+};
+
+export default AboutScreen;
---
+++ screens/ContactScreen.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { ScrollView } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const ContactScreen = () => {
+
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    );
+};
+
+export default ContactScreen;
