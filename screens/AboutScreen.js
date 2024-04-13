5000f1024419c92b26d513b4cbd92631ce6c253a
lgenteman <lydiagenteman@gmail.com>
2024-04-13 18:28:52 -0500

    Fix Mission Card in AboutScreen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import {Avatar, ListItem, Card} from 'react-native-elements';
 import { ScrollView, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { PARTNERS } from "../shared/partners";
 
 const Mission = () => {
     return (
             <Card>
                 <Card.Title>Our Mission</Card.Title>
-                <Card.Divider></Card.Divider>
-                <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+                    <Card.Divider></Card.Divider>
+                        <Card.Content>
+                            <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+                        </Card.Content>
             </Card>
     );
 };
